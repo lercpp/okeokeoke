3e0759ce7df4ee091aad9ad5f924b2546e659c92
lercpp <flashiap08@gmail.com>
2025-04-09 13:05:52 +0300

    дз готово

--- okeokeoke.py
+++ okeokeoke.py
@@ -4,33 +4,45 @@ class Vector2D:
         self.x=x
         self.y=y
 
-    #кринж
-        
-
+    def __add__(self,other:Vector2D):
+        return Vector2D(self.x+other.x,self.y+other.y)
+    
+    def __sub__(self,other:Vector2D):
+        return Vector2D(self.x-other.x,self.y-other.y)
+    
+    def __mul__(self,scalar:float):
+        return Vector2D(self.x*scalar,self.y*scalar)
+    
+    def __len__(self):
+        return float(self.x**2+self.y**2)
+    
+    def __str__(self): 
+        return f"{self.x};{self.y}"
+    
 
 class Money:
 
     def __init__(self,dollars:int,cents:int):
         self.dollars=dollars
         self.cents=cents
-
-    def add(self,second_money):#тут она еще не поняла ничего
-        cents=self.cents//100  #копейки лишнее
-        cents_2=self.second_money//100
-        sum=self.dollars+cents+second_money.dollars+cents_2
-        return sum
     
-    def __add__(self,other):
-        return self.add(other)
+    def __add__(self,other: Money):
+        d=self.dollars+other.dollars
+        c=self.cents+other.cents
+        while c!=99:
+            if c>=100:
+                d+=1
+                c-=100
+        return Money(d,c)
     
-    def sub(self,second_money):
-        cents=self.cents//100
-        cents_2=self.second_money//100
-        sub=(self.dollars+cents)-(second_money.dollars+cents_2)
-        return sub
-    
-    def __sub__(self,other):
-        return self.sub(other)
+    def __sub__(self,other:Money):
+        d=self.dollars-other.dollars
+        c=self.cents-other.cents
+        while c!=99:
+            if c>=100:
+                d-=1
+                c-=100
+        return Money(d,c)
     
     def __repr__(self):
         return f"dollars {self.dollars} and cents {self.cents}"
@@ -50,41 +62,46 @@ class Time:
         self.minutes=minutes
         self.seconds=seconds
 
-    def add(self,second_time):
-        seconds=(self.seconds//60)+(second_time.seconds//60)
-        minutes=(self.minutes//60+second_time.minutes)+(seconds//60)
-        hours=self.hours+minutes+second_time.hours
-        return hours
+    def __add__(self, other: Time):
+
+        h = self.h+other.h
+        m = self.m+other.m
+        s = self.s+other.s
+        while s!=59:
+            if(s>=60):
+                m+=1
+                s-=60
+        while m!=59:    
+            if(m>=60):
+                h+=1
+                m-=60
+            
+        return Time(h,m,s)
     
-    def __add__(self,other):
-        return self.add(other)
-    
-    def len(self,second_time):
-        hours=(self.hours//60)+(second_time.hours//60)
-        minutes=(self.minutes//60+second_time.minutes)+(hours//60)
-        seconds=self.seconds+minutes+second_time.seconds
-        return seconds
+    def __len__(self):
+        return self.s+self.m*60+self.h*3600
     
     def __repr__(self):
-        return f" {self.hours} , {self.minutes} , {self.seconds}"
+        return f" {self.hours},{self.minutes},{self.seconds}"
     
 a=Time(6,8,9)
 b=Money(8,8,8)
 
 print(a+b)
-print(a.len(b))
+print(len(a))
 print(repr(a))
+
 
 class Point:
     def __init__(self,x:int,y:int):
         self.x=x
         self.y=y
 
-    def __add__(self,other):#тут она прозрела
-        return other+self.x+self.y
+    def __add__(self,other:Point):
+        return Point(other.x+self.x,self.y+other.y)
         
-    def __sub__(self,other):
-        return other-self.x-self.y
+    def __sub__(self,other:Point):
+        return Point(other.x-self.x,other.y-self.y)
     
     def __repr__(self):
         return f"{self.x},{self.y}"
@@ -96,26 +113,27 @@ print(a+b)
 print(a-b)
 print(repr(a))
 
-class ColoredPoint:
 
-    def __init__(self,x:int,y:int,color:str):
-        self.x=x
-        self.y=y
+class ColoredPoint(Point):
+
+    def __init__(self,color:str):
         self.color=color
 
-    def __add__(self,other):
-        if self.color==self.color:
-            return other+self.x+self.y
+    def __add__(self,other: ColoredPoint):
+        point: Point=super().__add__(other) 
+        if self.color==other.color:
+            a=self.color  
+        else: 
+            a='black'          
+        return ColoredPoint(point.x, point.y,a)
         
-        else:
-            return f"blac {other+self.x+self.y}"
-        
-    def __sub__(self,other):
-        if self.color==self.color:
-            return other-self.x-self.y
-        
-        else:
-            return f"blac {other-self.x-self.y}"
+    def __sub__(self,other: ColoredPoint):
+        point: Point=super().__sub__(other)
+        if self.color==other.color:
+            a=self.color  
+        else: 
+            a='black'        
+        return ColoredPoint(point.x, point.y,a)
         
     def __repr__(self):
         return f"{self.x},{self.y},{self.color}"
@@ -127,6 +145,7 @@ print(a+b)
 print(a-b)
 print(repr(a))
 
+
 class Matrix:
 
     def __init__(self,a:int,b:int,c:int,d:int):
@@ -135,12 +154,14 @@ class Matrix:
         self.c=c
         self.d=d
 
-    def __add__(self,other_matrix):
-        return other_matrix+self.a+self.b+self.c+self.d
+    def __add__(self,other: Matrix):
+        return Matrix(self.a+other.a,self.b+other.b,self.c+other.c,self.d+other.d)
     
-    def __mul__(self,number):
-        a=number*(self.a+self.b+self.c+self.d)
-        return a
+    def __mul__(self,num: int):
+        return Matrix(self.a*num,self.b*num,self.c*num,self.d*num)
+    
+    def __len__(self):
+        return 4 
     
     def __repr__(self):
         return f"{self.a},{self.b},{self.c},{self.d}"
@@ -150,21 +171,23 @@ b=Matrix(1,2,3,4)
 
 print(a+b)
 print(a*b)
+print(len(a))
 print(repr(a))
+
 
 class Temperature:
 
     def __init__(self,degrees):
         self.degrees=degrees
 
-    def __add__(self,other):
-        return other+self.degrees
+    def __add__(self,other: Temperature):
+        return Temperature(other.degrees+self.degrees)
     
-    def __sub__(self,other):
-        return other-self.degrees
+    def __sub__(self,other:Temperature):
+        return Temperature(other.degrees-self.degrees)
     
-    def __mul__(self,factor):
-        a=factor*self.degrees
+    def __mul__(self,other:Temperature,factor:int):
+        a=factor*(self.degrees+other.degrees)
 
     def __repr__(self):
         return f"{self.degrees}"
